0805118c7e15f052e2156e91d9c07e2ba607b4ca
Mohammed Ennekhli <ennekhli@hotmail.com>
2021-07-31 13:42:59 +0100

    fixed order issues

--- src/app/shared/components/order-details/order-details.component.ts
+++ src/app/shared/components/order-details/order-details.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Observable, Subscription } from 'rxjs';
+import { Subscription } from 'rxjs';
 import { Order } from 'shared/models/order';
 import { OrderService } from 'shared/services/order.service';
 
--- src/app/shared/components/orders/orders.component.ts
+++ src/app/shared/components/orders/orders.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { SnapshotAction } from '@angular/fire/database';
 import { Observable } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
